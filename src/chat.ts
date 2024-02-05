e05064735b13e77f5c84e52315cf0a40602481d6
sayoi341 <me@sayoi341.moe>
2024-02-05 15:50:48 +0900

    fix

--- src/chat.ts
+++ src/chat.ts
@@ -12,103 +12,102 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanMessage, SystemMessage } from "langchain/schema";
 
 type Bindings = {
-	OPENAI_API_KEY: string;
-	BUCKET: R2Bucket;
-	USER: string;
-	PASS: string;
+  OPENAI_API_KEY: string;
+  BUCKET: R2Bucket;
+  USER: string;
+  PASS: string;
 };
 
 const chat = new Hono<{ Bindings: Bindings }>();
 
 chat.use("/", async (c, next) => {
-	const auth = basicAuth({ username: c.env.USER, password: c.env.PASS });
-	return auth(c, next);
+  const auth = basicAuth({ username: c.env.USER, password: c.env.PASS });
+  return auth(c, next);
 });
 
 chat.get("/", (c) => c.text("Auth OK"));
 
 chat.post(
-	"/",
-	zValidator(
-		"json",
-		z.object({
-			body: z.object({
-				image: z.string().refine(Base64.isValid),
-				langage: z.string(),
-			}),
-		}),
-	),
-	async (c) => {
-		const data = await c.req.json<{
-			body: { image: string; langage: string };
-		}>();
-		const base64 = data.body.image;
+  "/",
+  zValidator(
+    "json",
+    z.object({
+      image: z.string().refine(Base64.isValid),
+      language: z.string(),
+    })
+  ),
+  async (c) => {
+    const data = await c.req.json<{
+      image: string;
+      language: string;
+    }>();
+    const base64 = data.image;
 
-		const type = detectType(base64);
-		if (!type) return c.notFound();
+    const type = detectType(base64);
+    if (!type) return c.notFound();
 
-		const body = Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+    const body = Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
 
-		const key = `${await sha256(body)}.${type?.suffix}`;
+    const key = `${await sha256(body)}.${type?.suffix}`;
 
-		await c.env.BUCKET.put(key, body, {
-			httpMetadata: {
-				contentType: type?.mimeType,
-			},
-		});
+    await c.env.BUCKET.put(key, body, {
+      httpMetadata: {
+        contentType: type?.mimeType,
+      },
+    });
 
-		const vision = new ChatOpenAI({
-			openAIApiKey: c.env.OPENAI_API_KEY,
-			modelName: "gpt-4-vision-preview",
-			maxTokens: 300,
-		});
+    const vision = new ChatOpenAI({
+      openAIApiKey: c.env.OPENAI_API_KEY,
+      modelName: "gpt-4-vision-preview",
+      maxTokens: 300,
+    });
 
-		const text = await vision.invoke([
-			new SystemMessage({
-				content: [
-					{
-						type: "text",
-						text: "You are OCR machine. Output the characters in the given image. You can only respond with the extracted text.",
-					},
-				],
-			}),
-			new HumanMessage({
-				content: [
-					{
-						type: "image_url",
-						image_url: `https://image.sayoi341.moe/${key}`,
-					},
-				],
-			}),
-		]);
+    const text = await vision.invoke([
+      new SystemMessage({
+        content: [
+          {
+            type: "text",
+            text: "You are OCR machine. Output the characters in the given image. You can only respond with the extracted text.",
+          },
+        ],
+      }),
+      new HumanMessage({
+        content: [
+          {
+            type: "image_url",
+            image_url: `https://image.sayoi341.moe/${key}`,
+          },
+        ],
+      }),
+    ]);
 
-		const chat = new ChatOpenAI({
-			openAIApiKey: c.env.OPENAI_API_KEY,
-			modelName: "gpt-4",
-			maxTokens: 300,
-		});
+    const chat = new ChatOpenAI({
+      openAIApiKey: c.env.OPENAI_API_KEY,
+      modelName: "gpt-4",
+      maxTokens: 300,
+    });
 
-		const result = await chat.invoke([
-			new SystemMessage({
-				content: [
-					{
-						type: "text",
-						text: `Translate the text given by the user into ${data.body.langage}.`,
-					},
-				],
-			}),
-			new HumanMessage({
-				content: [
-					{
-						type: "text",
-						text: text.content.toString(),
-					},
-				],
-			}),
-		]);
+    const result = await chat.invoke([
+      new SystemMessage({
+        content: [
+          {
+            type: "text",
+            text: `Translate the text given by the user into ${data.language}.`,
+          },
+        ],
+      }),
+      new HumanMessage({
+        content: [
+          {
+            type: "text",
+            text: text.content.toString(),
+          },
+        ],
+      }),
+    ]);
 
-		return c.json(result.toJSON());
-	},
+    return c.json(result.toJSON());
+  }
 );
 
 export default chat;
